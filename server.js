ccfe9f406cf1283164bb784d42546efc2d939e36
Peter Kim <PeterLKim1337@gmail.com>
2016-08-26 00:47:17 -0700

    debugging

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var express = require('express');
 //Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
-//req.headers['x-forwarded-proto'] === 'http'
 
 if (PORT !== 3000) {
-  app.use(function (req, res, next) {
-    if (req.protocol === 'http') {
-      next();
-    } else {
-      res.redirect('http://' + req.hostname + req.url);
-    }
-  });
+    app.use(function(req, res, next) {
+        if (req.headers['x-forwarded-proto'] === 'https') {
+            res.redirect('http://' + req.hostname + req.url);
+
+        } else {
+            next();
+        }
+    });
 }
 
 app.use(express.static('public'));
