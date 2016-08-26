662ff550cfbc6a577e9d8ac806cf4328d607a01d
Peter Kim <PeterLKim1337@gmail.com>
2016-08-26 00:48:37 -0700

    debugging

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ if (PORT !== 3000) {
     app.use(function(req, res, next) {
         if (req.headers['x-forwarded-proto'] === 'https') {
             res.redirect('http://' + req.hostname + req.url);
-
         } else {
+            console.log('not an https');
             next();
         }
     });
