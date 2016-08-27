a3189386a4a4e335bd972b806cd3ff0db9bf1dbb
Peter Kim <PeterLKim1337@gmail.com>
2016-08-26 21:14:19 -0700

    added more foundation

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example locations to try out:</p>
+            <ol>
+                <li><Link to='/?location=Seattle'>Seattle, WA</Link></li>
+                <li><Link to='/?location=Seattle'>Rio, Brazil</Link></li>
+            </ol>
         </div>
     )
 };
